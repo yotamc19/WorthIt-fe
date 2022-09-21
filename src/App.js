f8a14e3afaf2753c8ea6941fb877795134cb72a0
leobh76 <lbhurovich@gmail.com>
2022-09-21 11:05:54 +0300

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+
 const App = () => {
-  return <div>Lets get coding!</div>;
-}
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import {
+  FaHome,
+  FaBell,
+  FaDollarSign,
+  FaUser,
+  FaSlidersH,
+} from "react-icons/fa";
+
+const NavBar = () => {
+  return (
+    <div>
+      <Tabs isFitted variant="enclosed">
+        <TabList mb="1em">
+          <Tab>
+            <Link to="/">
+              <FaHome />
+            </Link>
+          </Tab>
+          <Tab>
+            <Link to="/notifications">
+              <FaBell />
+            </Link>
+          </Tab>
+          <Tab>
+            <Link to="/mysalary">
+              <FaDollarSign />
+            </Link>
+          </Tab>
+          <Tab>
+            <Link to="/profile">
+              <FaUser />
+            </Link>
+          </Tab>
+          <Tab>
+            <Link to="/settings">
+              <FaSlidersH />
+            </Link>
+          </Tab>
+        </TabList>
+      </Tabs>
+    </div>
+  );
+};
+
+export default NavBar;
