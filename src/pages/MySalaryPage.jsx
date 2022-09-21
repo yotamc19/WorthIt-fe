85d844bb730691dee0841f7b35c896938c69ca16
leobh76 <lbhurovich@gmail.com>
2022-09-21 15:09:56 +0300

    small design changes on My Salary

--- src/pages/MySalaryPage.jsx
+++ src/pages/MySalaryPage.jsx
@@ -5,9 +5,8 @@ import Button from 'react-bootstrap/Button';
 const MySalaryPage = () => {
   return (
     <div className="d-flex flex-column justify-content-between align-items-center">
-      <div className="mt-3">WorthIt banner placeholder</div>
       <h1 className="fs-4 fw-bolder mt-3">My Salary</h1>
-      <div className="main-content bg-info p-4 mt-3">
+      <div className="main-content bg-white shadow rounded p-4 mt-4">
         <div className="salary-range">
           <h2 className="fs-2 fw-bolder text-center">
             Your Current Salary
@@ -20,14 +19,14 @@ const MySalaryPage = () => {
           <div className="salary-range text-center">
             <BsFillCaretUpFill className="mx-auto mt-3 fs-3 text-success" />
             <div className="top-value fs-4 fw-bolder">$ 9.5K</div>
-            <div className="d-flex flex-column align-items-center">
+            <div className="opacity-25 d-flex flex-column align-items-center">
               <FaEllipsisV />
               <FaEllipsisV />
               <FaEllipsisV />
             </div>
             <div className="bottom-value fs-4 fw-bolder">$ 7K</div>
-            <BsFillCaretDownFill className="mx-auto fs-3 text-danger" />
-            <Button variant="success" className="mb-4">I Want a New Estimate!</Button>
+            <BsFillCaretDownFill className="mx-auto mb-3 fs-3 text-danger" />
+            <Button variant="success" className="mb-3">I Want a New Estimate!</Button>
           </div>
         </div>
       </div>
